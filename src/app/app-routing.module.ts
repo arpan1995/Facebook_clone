24f7ee667c5384b4d4f400b4f4b9c28d06c137e4
Arpan Das <thearpandas47@gmail.com>
2023-07-21 01:41:20 +0530

    upd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './Components/login/login.component';
+import { HomeModule } from './Components/home/home.module';
+import { FacebookGuard } from './guards/facebook.guard';
+import { HomeComponent } from './Components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+  path:'', component:HomeComponent,
+  // loadChildren: () => import('./Components/home/home.module').then(m => m.HomeModule),
+  canActivate:[FacebookGuard]
+  },
+  {
+    path:'login', component:LoginComponent
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+function m(value: typeof import("c:/Users/arpan/OneDrive/Desktop/Angular projects/Facebook_Clone/src/app/Components/home/home.module")): typeof import("c:/Users/arpan/OneDrive/Desktop/Angular projects/Facebook_Clone/src/app/Components/home/home.module") | PromiseLike<typeof import("c:/Users/arpan/OneDrive/Desktop/Angular projects/Facebook_Clone/src/app/Components/home/home.module")> {
+  throw new Error('Function not implemented.');
+}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './Components/home/home.component';
+import { LoginComponent } from './Components/login/login.component';
+import { RegisterComponent } from './Components/register/register.component';
+import { MaterialModule } from './shared/material.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
